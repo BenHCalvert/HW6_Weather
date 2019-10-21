70bbaf31b8b944325875ee34f2644831a8e45e0e
Ben Calvert <benhcalvert@gmail.com>
2019-10-20 19:28:14 -0600

    fx for local storage and fx for onclick

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // UV Index
 var apiKey = 0457b30a523cda1e67defc7edc1045b8 ;
-var cityInput = 
+var cityInput = cityInput.val().trim();
 var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey ;
 var weatherURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityInput + "&units=metric&cnt=5" + APIKey;
 
@@ -13,3 +13,11 @@ $("#search").on('click', function () {
     // cityInput = cityInput.join('-');
     console.log(cityInput);
 });
+
+// city history to local storage.
+function storeCity(city) {
+    var currentCity = $('<li>').text(city);
+    currentCity.attr({type: 'button', class:'storeCity', name:city});
+    $('#cities').append(currentCity);
+    localStorage.setItem(city, city);
+};
